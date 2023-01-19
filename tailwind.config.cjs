81509a2e65ab31314523bcd310f4dbba4234d2c8
Griffin <griffinmmatthews@gmail.com>
2023-01-19 18:41:01 -0500

    format prettier rules on files

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+    future: {
+        hoverOnlyWhenSupported: true,
+    },
+    content: ['./src/**/*.{js,ts,jsx,tsx}'],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                primary: '#2F64C9',
+                secondary: '#9030A4',
+                light: '#EEEEEE',
+                'light-secondary': '#DEDEDE',
+                dark: '#222222',
+                'dark-secondary': '#333333',
+                error: 'red',
+            },
+        },
+    },
+    plugins: [],
+}
