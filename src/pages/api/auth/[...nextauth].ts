1085f31d78dbec1fc872ccf72c77f01dc6b1b63f
Griffin <griffinmmatthews@gmail.com>
2023-01-23 16:22:00 -0500

    add prisma scheme for pokemon

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -12,26 +12,17 @@ export const authOptions: NextAuthOptions = {
         session({ session, user }) {
             if (session.user) {
                 session.user.id = user.id
+                console.log(user)
             }
             return session
         },
     },
-    // Configure one or more authentication providers
     adapter: PrismaAdapter(prisma),
     providers: [
         DiscordProvider({
             clientId: env.DISCORD_CLIENT_ID,
             clientSecret: env.DISCORD_CLIENT_SECRET,
         }),
-        /**
-         * ...add more providers here
-         *
-         * Most other providers require a bit more work than the Discord provider.
-         * For example, the GitHub provider requires you to add the
-         * `refresh_token_expires_in` field to the Account model. Refer to the
-         * NextAuth.js docs for the provider you want to use. Example:
-         * @see https://next-auth.js.org/providers/github
-         */
     ],
 }
 
