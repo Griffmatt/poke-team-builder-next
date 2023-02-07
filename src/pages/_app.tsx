8c28252694ead7f2071a3219a052685fc7b1126e
Griffin <griffinmmatthews@gmail.com>
2023-02-07 14:53:00 -0500

    add scroll rstoration on router back

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,11 +7,15 @@ import { SessionProvider } from "next-auth/react"
 import { api } from "utils/api"
 
 import { Layout } from "components/ui/layout"
+import { useScrollRestoration } from "hooks/useScrollRestoration"
+import { useRouter } from "next/router"
 
 const MyApp: AppType<{ session: Session | null }> = ({
     Component,
     pageProps: { session, ...pageProps },
 }) => {
+    const router = useRouter()
+    useScrollRestoration(router)
     return (
         <SessionProvider session={session}>
             <Layout>
