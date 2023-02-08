7bef9154c10ebcdc81a461f756df6687a0fb65da
Griffin <griffinmmatthews@gmail.com>
2023-02-07 23:10:12 -0500

    update scroll restoration to not change on link click

--- src/hooks/useScrollRestoration.ts
+++ src/hooks/useScrollRestoration.ts
@@ -1,57 +1,62 @@
-import { useEffect } from 'react'
-import Router, { NextRouter } from 'next/router'
+import { useEffect, useState } from "react"
+
+import Router, { useRouter } from "next/router"
 
 function saveScrollPos(asPath: string) {
-  sessionStorage.setItem(
-    `scrollPos:${asPath}`,
-    JSON.stringify({ x: window.scrollX, y: window.scrollY })
-  )
+    sessionStorage.setItem(
+        `scrollPos:${asPath}`,
+        JSON.stringify({ x: window.scrollX, y: window.scrollY })
+    )
 }
 
 function restoreScrollPos(asPath: string) {
-  const json = sessionStorage.getItem(`scrollPos:${asPath}`)
-  const scrollPos = json ? JSON.parse(json) : undefined
-  if (scrollPos) {
-    window.scrollTo(scrollPos.x, scrollPos.y)
-  }
+    const json = sessionStorage.getItem(`scrollPos:${asPath}`)
+    const scrollPos = json ? JSON.parse(json) : undefined
+    if (scrollPos) {
+        window.scrollTo(scrollPos.x, scrollPos.y)
+    }
 }
 
-export function useScrollRestoration(router: NextRouter) {
-  useEffect(() => {
-    if (!('scrollRestoration' in window.history)) return
-    let shouldScrollRestore = false
-    window.history.scrollRestoration = 'manual'
-    restoreScrollPos(router.asPath)
+export const useScrollRestoration = (): void => {
+    const router = useRouter()
+    const [shouldScrollRestore, setShouldScrollRestore] = useState(false)
 
-    const onBeforeUnload = (event: BeforeUnloadEvent) => {
-      saveScrollPos(router.asPath)
-      delete event['returnValue']
-    }
+    useEffect(() => {
+        if (shouldScrollRestore) {
+            restoreScrollPos(router.asPath)
+        }
+    }, [router.asPath, shouldScrollRestore])
 
-    const onRouteChangeStart = () => {
-      saveScrollPos(router.asPath)
-    }
+    useEffect(() => {
+        if (!("scrollRestoration" in window.history)) return
+        window.history.scrollRestoration = "manual"
 
-    const onRouteChangeComplete = (url: string) => {
-      if (shouldScrollRestore) {
-        shouldScrollRestore = false
-        restoreScrollPos(url)
-      }
-    }
+        const onBeforeUnload = (event: BeforeUnloadEvent) => {
+            saveScrollPos(router.asPath)
+            delete event["returnValue"]
+        }
 
-    window.addEventListener('beforeunload', onBeforeUnload)
-    Router.events.on('routeChangeStart', onRouteChangeStart)
-    Router.events.on('routeChangeComplete', onRouteChangeComplete)
-    Router.beforePopState(() => {
-      shouldScrollRestore = true
-      return true
-    })
+        const onRouteChangeStart = () => {
+            saveScrollPos(router.asPath)
+        }
 
-    return () => {
-      window.removeEventListener('beforeunload', onBeforeUnload)
-      Router.events.off('routeChangeStart', onRouteChangeStart)
-      Router.events.off('routeChangeComplete', onRouteChangeComplete)
-      Router.beforePopState(() => true)
-    }
-  }, [router])
+        const onRouteChangeComplete = () => {
+            setShouldScrollRestore(false)
+        }
+
+        window.addEventListener("beforeunload", onBeforeUnload)
+        Router.events.on("routeChangeStart", onRouteChangeStart)
+        Router.events.on("routeChangeComplete", onRouteChangeComplete)
+        Router.beforePopState(() => {
+            setShouldScrollRestore(true)
+            return true
+        })
+
+        return () => {
+            window.removeEventListener("beforeunload", onBeforeUnload)
+            Router.events.off("routeChangeStart", onRouteChangeStart)
+            Router.events.off("routeChangeComplete", onRouteChangeComplete)
+            Router.beforePopState(() => true)
+        }
+    }, [router, shouldScrollRestore])
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,14 +8,12 @@ import { api } from "utils/api"
 
 import { Layout } from "components/ui/layout"
 import { useScrollRestoration } from "hooks/useScrollRestoration"
-import { useRouter } from "next/router"
 
 const MyApp: AppType<{ session: Session | null }> = ({
     Component,
     pageProps: { session, ...pageProps },
 }) => {
-    const router = useRouter()
-    useScrollRestoration(router)
+    useScrollRestoration()
     return (
         <SessionProvider session={session}>
             <Layout>
