fd266d6e4d840834e090e135b2bdea1038933e74
Griffin <griffinmmatthews@gmail.com>
2023-02-16 15:10:45 -0500

    move popular teams and pokemon to boxes

---
+++ src/components/boxes/recentTeams.tsx
@@ -0,0 +1,44 @@
+import { TeamRows } from "components/teams/teamRows"
+import { SkeletonTeamRows } from "components/teams/ui/skeletonTeamRows"
+import { useSession } from "next-auth/react"
+import { api } from "utils/api"
+
+export const RecentTeams = () => {
+    const { data: session } = useSession()
+
+    const { data: teams, isLoading, error } = api.teams.recentTeams.useQuery()
+
+    const {
+        data: favorites,
+        isLoading: isLoading2,
+        error: error2,
+        isFetching,
+    } = api.favorite.checkUserFavoriteTeams.useQuery(
+        {
+            userId: session?.user?.id as string,
+        },
+        { enabled: !!session?.user?.id }
+    )
+
+    if (isLoading || (isLoading2 && isFetching)) {
+        return (
+            <div className="grid gap-3">
+                <h2>Recent Teams</h2>
+                <SkeletonTeamRows />
+            </div>
+        )
+    }
+    if (error) return <div>Error: {error.message}</div>
+    if (error2) return <div>Error: {error2.message}</div>
+
+    return (
+        <>
+            {teams.length > 0 && (
+                <div className="grid gap-3">
+                    <h2>Recent Teams</h2>
+                    <TeamRows teams={teams} favoriteTeams={favorites} />
+                </div>
+            )}
+        </>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,6 @@ import Link from "next/link"
 import { signIn, useSession } from "next-auth/react"
 import { PokemonDataGrid } from "components/pokemonGrids/pokemonDataGrid"
 import { SkeletonPokemonGrid } from "components/pokemonGrids/ui/skeletonPokemonGrid"
-import { CreatedPokemonGrid } from "components/pokemonGrids/createdPokemonGrid"
-import { TeamRows } from "components/teams/teamRows"
-import { SkeletonTeamRows } from "components/teams/ui/skeletonTeamRows"
 
 const Home: NextPage = () => {
     const {
@@ -25,8 +22,10 @@ const Home: NextPage = () => {
                 </div>
                 <div className="grid gap-3">
                     <HomepageButtons />
-                    <PopularPokemon />
-                    <PopularTeams />
+                    <div className="grid gap-3">
+                        <h2>Team of the Week</h2>
+                        <p>Coming soon!</p>
+                    </div>
                 </div>
             </main>
         )
@@ -43,8 +42,10 @@ const Home: NextPage = () => {
             </div>
             <div className="grid gap-3">
                 <HomepageButtons />
-                <PopularPokemon />
-                <PopularTeams />
+                <div className="grid gap-3">
+                    <h2>Team of the Week</h2>
+                    <p>Coming soon!</p>
+                </div>
             </div>
         </main>
     )
@@ -96,94 +97,6 @@ const HomepageButtons = () => {
                     </>
                 )}
             </div>
-        </>
-    )
-}
-
-const PopularPokemon = () => {
-    const { data: session } = useSession()
-    const pokemonGridAmount = 12
-
-    const {
-        data: popularPokemon,
-        isLoading,
-        error,
-    } = api.statistics.getPopularPokemon.useQuery()
-
-    const {
-        data: favorites,
-        isLoading: isLoading2,
-        error: error2,
-        isFetching,
-    } = api.favorite.checkUserFavoritePokemon.useQuery(
-        {
-            userId: session?.user?.id as string,
-        },
-        { enabled: !!session?.user?.id }
-    )
-
-    if (isLoading || (isLoading2 && isFetching)) {
-        return (
-            <div className="grid gap-3">
-                <h2>Popular Pokemon</h2>
-                <SkeletonPokemonGrid amount={pokemonGridAmount} />
-            </div>
-        )
-    }
-    if (error) return <div>Error: {error.message}</div>
-    if (error2) return <div>Error: {error2.message}</div>
-
-    return (
-        <div className="grid gap-3">
-            <h2>Popular Pokemon</h2>
-            <CreatedPokemonGrid
-                pokemons={popularPokemon?.slice(0, pokemonGridAmount)}
-                currentUserFavorites={favorites}
-            />
-        </div>
-    )
-}
-
-const PopularTeams = () => {
-    const { data: session } = useSession()
-
-    const {
-        data: teams,
-        isLoading,
-        error,
-    } = api.statistics.getPopularTeams.useQuery()
-
-    const {
-        data: favorites,
-        isLoading: isLoading2,
-        error: error2,
-        isFetching,
-    } = api.favorite.checkUserFavoriteTeams.useQuery(
-        {
-            userId: session?.user?.id as string,
-        },
-        { enabled: !!session?.user?.id }
-    )
-
-    if (isLoading || (isLoading2 && isFetching)) {
-        return (
-            <div className="grid gap-3">
-                <h2>Popular Teams</h2>
-                <SkeletonTeamRows />
-            </div>
-        )
-    }
-    if (error) return <div>Error: {error.message}</div>
-    if (error2) return <div>Error: {error2.message}</div>
-
-    return (
-        <>
-            {teams.length > 0 && (
-                <div className="grid gap-3">
-                    <h2>Popular Teams</h2>
-                    <TeamRows teams={teams} favoriteTeams={favorites} />
-                </div>
-            )}
         </>
     )
 }
