2d49e6ee56e5b0febb19f578f19ac09feef46c37
Griffin <griffinmmatthews@gmail.com>
2023-02-20 21:09:36 -0500

    remove h-full

--- src/components/ui/loadingSpinner.tsx
+++
@@ -1,7 +0,0 @@
-export const LoadingSpinner = () => {
-    return (
-        <div className="flex h-full w-full items-center justify-center">
-            <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-dark/5"></div>
-        </div>
-    )
-}
