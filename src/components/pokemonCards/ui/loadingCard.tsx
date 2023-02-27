b88952d7339e00a8bd8e2f7ed099093364ac27b8
Griffin <griffinmmatthews@gmail.com>
2023-02-27 00:01:30 -0500

    fix ui bugs

--- src/components/pokemonCards/ui/loadingCard.tsx
+++ src/components/pokemonCards/ui/loadingCard.tsx
@@ -5,7 +5,7 @@ export const LoadingCard = () => {
         <div className="grid gap-1 md:gap-3 lg:gap-5">
             <SkeletonRoundImage />
             <div className="grid place-items-end">
-                <h3 className="mb-1 h-4 w-24 animate-pulse bg-dark-3 md:h-5" />
+                <h3 className="mb-1 h-4 w-24 animate-pulse bg-dark-3 lg:h-5" />
                 <p className="h-4 w-10 animate-pulse bg-dark-3 md:h-5" />
             </div>
         </div>
--- src/components/teams/ui/skeletonTeamRow.tsx
+++ src/components/teams/ui/skeletonTeamRow.tsx
@@ -4,7 +4,7 @@ export const SkeletonTeamRow = ({ amount = 6 }) => {
     const fillerArr = Array.from({ length: amount }, () => 0)
     return (
         <div className="grid gap-3">
-            <h3 className="h-6 w-32 animate-pulse bg-dark-3" />
+            <h3 className="h-5 w-32 animate-pulse bg-dark-3 lg:h-6" />
             <div className="pokemon-team-row">
                 {fillerArr.map((_, index) => (
                     <div className="w-full p-2 md:p-3" key={index}>
