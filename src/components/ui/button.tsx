e23bff572d1326f8af55babf9caedecff0c090c8
Griffin <griffinmmatthews@gmail.com>
2023-02-28 16:35:56 -0500

    make height of type icons bigger inskeleton to match actual size

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,15 +1,13 @@
 interface button extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     text: string
-    secondaryText?: string
 }
 
-export const Button = ({ text, secondaryText, ...props }: button) => {
+export const Button = ({ text, ...props }: button) => {
     const bgColor = props.className ?? "bg-dark-3"
 
     return (
         <button {...props} className={`${bgColor} text-light `}>
             <h3>{text}</h3>
-            {secondaryText && <p>{secondaryText}</p>}
         </button>
     )
 }
