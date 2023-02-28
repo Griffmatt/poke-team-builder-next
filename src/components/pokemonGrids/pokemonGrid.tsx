79ab5dbe9e5e6b5e954f41e394766303dd911c3a
Griffin <griffinmmatthews@gmail.com>
2023-02-27 19:24:00 -0500

    update type of pokemon in pokemon grid

--- src/components/pokemonGrids/pokemonGrid.tsx
+++ src/components/pokemonGrids/pokemonGrid.tsx
@@ -1,6 +1,6 @@
 import { useInfiniteScroll } from "hooks/useInfiniteScroll"
 import Link from "next/link"
-import { type Pokemon } from "types/trpc"
+import { type Pokemon } from "pokenode-ts"
 
 import { PokemonCard } from "../pokemonCards/pokemonCard"
 import { PokemonEmpty } from "./ui/pokemonEmpty"
