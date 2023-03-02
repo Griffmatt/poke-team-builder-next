d0b3b0c50242c3b091d5908777812abc59f23de7
Griffin <griffinmmatthews@gmail.com>
2023-03-02 16:19:22 -0500

    add user stats api route and updated page to reflect new values

--- src/server/api/routers/statistics.ts
+++ src/server/api/routers/statistics.ts
@@ -1,3 +1,4 @@
+import { z } from "zod"
 import { countStringArr } from "../../../utils/countStringArr"
 import { formatTeams } from "../../utils/formatTeams"
 import { pokemonInclude, teamsInclude } from "../../utils/includeConfigs"
@@ -33,4 +34,40 @@ export const statisticsRouter = createTRPCRouter({
 
         return teamsFormatted.slice(0, 5)
     }),
+    getUserStats: publicProcedure
+        .input(z.object({ userId: z.string() }))
+        .query(async ({ ctx, input }) => {
+            const teams = await ctx.prisma.team.findMany({
+                where: {
+                    userId: input.userId,
+                },
+            })
+            const pokemons = await ctx.prisma.createdPokemon.findMany({
+                where: {
+                    userId: input.userId,
+                },
+                ...pokemonInclude,
+            })
+
+            const totalTeams = teams.length ?? 0
+            const totalPokemon = pokemons.length ?? 0
+            const wins = teams.reduce((total, team) => {
+                return total + team.wins
+            }, 0)
+            const favoriteTeam = teams.reduce((topTeam, team) => {
+                if (team.battles > topTeam.battles) return team
+                return topTeam
+            }, teams[0])
+            const pokemonNames = pokemons.map((pokemon) => pokemon.name)
+
+            const pokemonCounted = countStringArr(pokemonNames)
+            const favoritePokemon = pokemonCounted.string[0].name
+            return {
+                wins,
+                totalPokemon,
+                totalTeams,
+                favoriteTeam,
+                favoritePokemon,
+            }
+        }),
 })
