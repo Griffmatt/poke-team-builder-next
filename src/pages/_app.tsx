c6f3556898cfcf4ea4f06372c4b5a27858a26d4d
Griffin <griffinmmatthews@gmail.com>
2023-03-13 16:11:12 -0400

    add error boundary

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,7 @@ import { api } from "utils/api"
 import { Layout } from "components/ui/layout"
 import { useScrollRestoration } from "hooks/useScrollRestoration"
 import { SelectedContextProvider } from "context/selectedContext"
+import ErrorBoundary from "components/errorBoundary"
 
 const MyApp: AppType<{ session: Session | null }> = ({
     Component,
@@ -18,11 +19,13 @@ const MyApp: AppType<{ session: Session | null }> = ({
     useScrollRestoration()
     return (
         <SessionProvider session={session}>
-            <Layout>
-                <SelectedContextProvider>
-                    <Component {...pageProps} />
-                </SelectedContextProvider>
-            </Layout>
+            <ErrorBoundary>
+                <Layout>
+                    <SelectedContextProvider>
+                        <Component {...pageProps} />
+                    </SelectedContextProvider>
+                </Layout>
+            </ErrorBoundary>
         </SessionProvider>
     )
 }
