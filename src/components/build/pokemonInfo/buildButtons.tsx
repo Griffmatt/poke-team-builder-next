fb0532b7c4e13dccfc430724543aa6bd570d1a20
Griffin <griffinmmatthews@gmail.com>
2023-03-17 14:33:50 -0400

    update build pre select layout so if no user see builds button is on right and not in middle

--- src/components/build/pokemonInfo/buildButtons.tsx
+++ src/components/build/pokemonInfo/buildButtons.tsx
@@ -21,18 +21,13 @@ export const BuildButtons = ({ pokemonName }: Props) => {
             resetData()
         }
     }, [hasBuilds, resetData])
+
     return (
-        <div
-            className={`grid gap-3 ${
-                hasBuilds
-                    ? "xs:col-span-2 xs:grid-cols-2 lg:col-start-2"
-                    : "xs:col-start-2 lg:col-start-3"
-            }`}
-        >
+        <div className="col-span-full flex flex-col justify-end gap-3 sm:flex-row">
             {hasBuilds && (
                 <Link
                     href={`/build/pokemon/${pokemonName}/builds`}
-                    className="grid h-fit w-full place-items-center rounded-2xl bg-dark-3 px-4 py-2 text-center"
+                    className="w-full rounded-2xl bg-dark-3 px-4 py-2 text-center lg:w-[33%]"
                 >
                     <h3>See Builds</h3>
                 </Link>
@@ -40,7 +35,7 @@ export const BuildButtons = ({ pokemonName }: Props) => {
             {session?.user?.id && (
                 <Link
                     href={`/build/pokemon/${pokemonName}/build`}
-                    className="grid h-fit w-full  place-items-center rounded-2xl bg-dark-3 px-4 py-2 text-center"
+                    className="w-full rounded-2xl bg-dark-3 px-4 py-2 text-center lg:w-[33%]"
                 >
                     <h3>Build Pokemon</h3>
                 </Link>
